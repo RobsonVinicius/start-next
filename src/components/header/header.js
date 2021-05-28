c48185ca269f263cca43189c8683efdc74774ea3
Robson Vinicius <robsondgn@outlook.com>
2021-05-28 02:32:17 -0300

    header - position: Logo, nav: header.data, Button | react-scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,7 +9,27 @@ import menuItems from './header.data';
 
 export default function Header({ className }) {
   return (
-      <h1>Header</h1>
+      <header sx={styles.header} className={className} id="header">
+        <Container sx={styles.container}>
+          <Logo src={LogoDark} />
+          <Flex as="nav" sx={styles.nav}>
+            {menuItems.map((menuItem, i) => (
+              <Link
+                activeClass="active"
+                to={menuItem.path}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                key={i}
+              >
+                {menuItem.label}
+              </Link>
+            ))}
+          </Flex>
+          <Button className="donate__btn" variant="secondary" aria-label="Get Started">Get Started</Button>
+        </Container>
+      </header>
   );
 }
 
