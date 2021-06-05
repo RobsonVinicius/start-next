6c5dda3157393d8f79eb2a187f36b6ddaf053116
Robson Vinicius <robsondgn@outlook.com>
2021-06-05 01:05:56 -0300

    Feature Section

--- src/components/feature-card.js
+++ src/components/feature-card.js
@@ -8,7 +8,17 @@ export default function FeatureCard({
   text,
 }) {
   return (
-    <h1>FeatureCard</h1>
+    <Box sx={styles.card}>
+      <Image 
+        src={src}
+        altText={altText}
+        sx={styles.img}
+      />
+      <Box sx={styles.wrapper}>
+        <Heading sx={styles.wrapper.title}>{ title }</Heading>
+        <Text sx={styles.wrapper.subTitle}>{ text }</Text>
+      </Box>
+    </Box>
 
   );
 }
