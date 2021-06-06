1dfb03f61f3a8d6d0ffcbf6706e36e616694f274
Robson Vinicius <robsondgn@outlook.com>
2021-06-06 17:28:14 -0300

    Team Section

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -179,7 +179,26 @@ const data = [
 
 export default function TeamSection() {
   return (
-    <h1>Team Section</h1>
+    <section>
+      <Container>
+        <SectionHeader 
+          slogan="our team"
+          title="The most qualified and talented individuals"
+        />
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <TeamCard 
+              key={item.id}
+              src={item.imgSrc}
+              altText={item.altText}
+              title={item.title}
+              designation={item.designation}
+              social={item.socialProfile}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
