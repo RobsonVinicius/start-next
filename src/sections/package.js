efddcc32f0de604a7d35e147ab78c6fb046aa53b
Robson Vinicius <robsondgn@outlook.com>
2021-06-06 02:30:12 -0300

    Pricing Section

--- src/sections/package.js
+++ src/sections/package.js
@@ -242,6 +242,19 @@ const responsive = {
 export default function Package() {
   const { monthly, annual } = packages;
 
+  const [state, setState] = useState({
+    active: 'monthly',
+    pricingPlan: monthly,
+  })
+
+  const handlePricingPlan = (plan) => {
+    if(plan === 'annual') {
+      setState({ active: 'annual', pricingPlan: annual })
+    } else {
+      setState({ active: 'monthly', pricingPlan: monthly })
+    }
+  }
+
   const sliderParams = {
     additionalTransfrom: 0,
     arrows: false,
@@ -266,7 +279,43 @@ export default function Package() {
   };
 
   return (
-    <h1>Package</h1>
+    <section id="pricing" sx={{ variant: 'section.pricing' }}>
+      <Container>
+        <SectionHeader 
+          slogan="Pricing Plan"
+          title="Choose your pricing plan"
+        />
+        <Flex sx={styles.buttonGroup}>
+          <Box sx={styles.buttonGroupInner}>
+            <button
+              className={state.active === 'monthly' ? 'active' : ''}
+              type="button"
+              aria-label="Monthly"
+              onClick={() => handlePricingPlan('monthly')}
+            >
+              Monthly Plan
+            </button>
+            <button
+              className={state.active === 'annual' ? 'active' : ''}
+              type="button"
+              aria-label="Annual"
+              onClick={() => handlePricingPlan('annual')}
+            >
+              Annual Plan
+            </button>
+          </Box>
+        </Flex>
+        <Box sx={styles.pricingWrapper} className="pricing__wrapper">
+          <Carousel {...sliderParams}>
+            {state.pricingPlan.map((packageData) => (
+              <Box sx={styles.pricingItem} key={packageData.id}>
+                <PriceCard data={packageData} />
+              </Box>
+            ))}
+          </Carousel>
+        </Box>
+      </Container>
+    </section>
   );
 }
 
