fd346e6348e452a14ac9e60ed72a6369b397441c
Robson Vinicius <robsondgn@outlook.com>
2021-06-06 18:02:52 -0300

    Footer Section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,31 @@ import FooterLogo from 'assets/logo.svg';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={FooterLogo} alt="Logo" />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link 
+                  path={item.path}
+                  key={i}
+                  label={item.label}
+                  sx={styles.footer.link}
+                />
+              ))}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>
+            Copyright by {new Date().getFullYear()} | StartNext
+          </Text>
+        </Box>
+      </Container>
+      
+    </footer>
   );
 }
 
